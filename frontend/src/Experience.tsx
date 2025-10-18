89584b5c37a8c77c01303301ce2775d1e6cae175
c4bae <123281198+c4bae@users.noreply.github.com>
2025-10-18 01:49:33 -0400

    add lines to resemble a more handlike shape

--- frontend/src/Experience.tsx
+++ frontend/src/Experience.tsx
@@ -1,10 +1,14 @@
 import { OrbitControls } from "@react-three/drei"
 import { useEffect, useState } from "react"
 import "@react-three/fiber"
+import { Line } from "@react-three/drei"
 
 interface landmarkData {
     landmarks: number[][]; // declare the type that landmarks can have two array (for indexing) with numbers as each element
 }
+
+// define a type cords that takes in 3 values as an array
+type cords = [number, number, number]
 
 export default function Experience() {
     const [landmarkData, setLandmarkData] = useState<landmarkData | null>(null!)
@@ -24,23 +28,67 @@ export default function Experience() {
     return (
         <>
             <OrbitControls></OrbitControls>
-            {[...Array(21)].map((_, index) => // map iterates through undefined elements, not empty elements
-                {   
-                    if(landmarkData && landmarkData.landmarks) {
-                        const cords: [number, number, number] = 
-                        [(-landmarkData.landmarks[index][0] - 1280/2) / (1280/2) + 2, 
-                         -(landmarkData.landmarks[index][1] - 720/2) / (720/2) + 1, 
-                         landmarkData.landmarks[index][2]/200];
-                        
-                        return (
-                        <mesh key={index} position={[...cords]} scale={0.09}>
-                            <sphereGeometry></sphereGeometry>
-                            <meshStandardMaterial color={"blue"}></meshStandardMaterial>
-                        </mesh>
-                    )
+            <group>
+                {[...Array(21)].map((_, index) => // map iterates through undefined elements, not empty elements
+                    {   
+                        if(landmarkData && landmarkData.landmarks) {
+                            const x = -(landmarkData.landmarks[index][0] - 1280/2) / (1280/2) * 3
+                            const y = -(landmarkData.landmarks[index][1] - 720/2) / (720/2) + 0.5
+                            const z = landmarkData.landmarks[index][2]/200
+                            const cords: cords = [x,y,z]
+
+                            let nextX, nextY, nextZ
+                            let nextCords: cords;
+                            
+                            if(index != 20 && index % 4 != 0) {
+                                nextX = -(landmarkData.landmarks[index+1][0] - 1280/2) / (1280/2) * 3
+                                nextY = -(landmarkData.landmarks[index+1][1] - 720/2) / (720/2) + 0.5
+                                nextZ = landmarkData.landmarks[index+1][2]/200
+                                nextCords = [nextX, nextY, nextZ]
+                            }
+                            const zeroPoint: cords = [
+                                -(landmarkData.landmarks[0][0] - 1280/2) / (1280/2) * 3,
+                                -(landmarkData.landmarks[0][1] - 720/2) / (720/2) + 0.5,
+                                landmarkData.landmarks[0][2]/200
+                            ]
+
+                            const ninethPoint: cords = [
+                                -(landmarkData.landmarks[9][0] - 1280/2) / (1280/2) * 3,
+                                -(landmarkData.landmarks[9][1] - 720/2) / (720/2) + 0.5,
+                                landmarkData.landmarks[9][2]/200,
+                            ]
+
+                            const thirteenthPoint: cords = [
+                                -(landmarkData.landmarks[13][0] - 1280/2) / (1280/2) * 3,
+                                -(landmarkData.landmarks[13][1] - 720/2) / (720/2) + 0.5,
+                                landmarkData.landmarks[13][2]/200,
+                            ]
+
+                            const seventeenthPoint: cords = [
+                                -(landmarkData.landmarks[17][0] - 1280/2) / (1280/2) * 3,
+                                -(landmarkData.landmarks[17][1] - 720/2) / (720/2) + 0.5,
+                                landmarkData.landmarks[17][2]/200,
+                            ]
+
+                            return (
+                            <group> 
+                                {nextCords && <Line points={[[...cords], [...nextCords]]} lineWidth={5} color={"blue"}></Line>}
+                                {index == 1 && <Line points={[[...cords], [...zeroPoint]]} lineWidth={5} color={"blue"}></Line>}
+                                {index == 5 && <Line points={[[...cords], [...zeroPoint]]} lineWidth={5} color={"blue"}></Line>}
+                                {index == 17 && <Line points={[[...cords], [...zeroPoint]]} lineWidth={5} color={"blue"}></Line>}
+                                {index == 5 && <Line points={[[...cords], [...ninethPoint]]} lineWidth={5} color={"blue"}></Line>}
+                                {index == 9 && <Line points={[[...cords], [...thirteenthPoint]]} lineWidth={5} color={"blue"}></Line>}
+                                {index == 13 && <Line points={[[...cords], [...seventeenthPoint]]} lineWidth={5} color={"blue"}></Line>}
+                                <mesh key={index} position={[...cords]} scale={0.09} castShadow>
+                                    <sphereGeometry></sphereGeometry>
+                                    <meshStandardMaterial color={"blue"}></meshStandardMaterial>
+                                </mesh>
+                            </group >
+                        )
+                        }
                     }
-                }
-            )}
+                )}
+            </group>
 
             <mesh position-y={-1} scale={10} rotation-x={-Math.PI/2} receiveShadow>
                 <planeGeometry></planeGeometry>
