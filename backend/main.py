f6ffaa6d5445855940827c82a56cc55e67cb3128
c4bae <123281198+c4bae@users.noreply.github.com>
2025-10-17 21:09:14 -0400

    send landmark data over websocket

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+import asyncio, json, websockets
 
 # Webcam
 cap = cv2.VideoCapture(0) # 0 refers to default webcam
@@ -9,19 +10,33 @@ cap.set(4, 720)  # Prop id 4 refers to video height
 # Hand Detector
 detector = HandDetector(maxHands=1, detectionCon=0.8)
 
-while True:
-    # Get the frame from the webcam
-    success, img = cap.read()
-    if not success:
-        break  # break if program fails to grab frame or recognize webcam
-    hands, img = detector.findHands(img)  # hands contains information about hands
 
-    # Landmark values - (x,y,z) * 21 --> total number of values
-    if hands:
-        # Get the first hand
-        hand = hands[0]
-        # Get the landmark list (list of 21 lists of landmark cords)
-        lmList = hand["lmList"]
+async def hand_landmarks(websocket):
+    while True:
+        # Get the frame from the webcam
+        success, img = cap.read()
+        if not success:
+            break  # break if program fails to grab frame or recognize webcam
+        hands, img = detector.findHands(img)  # hands contains information about hands
+        data = {}
 
-    cv2.imshow("Image", img)  # imshow short for image show, "Image" is window name, img is images
-    cv2.waitKey(1)  # Make video refresh every 1 ms
+        # Landmark values - (x,y,z) * 21 --> total number of values
+        if hands:
+            # Get the first hand
+            hand = hands[0]
+            # Get the landmark list (list of 21 lists of landmark cords)
+            lmList = hand["lmList"]
+            data["landmarks"] = lmList
+
+        cv2.imshow("Image", img)  # imshow short for image show, "Image" is window name, img is images
+        cv2.waitKey(1)  # Make video refresh every 1 ms
+
+        await websocket.send(json.dumps(data))
+        await asyncio.sleep(0.02)
+
+        
+async def main():
+    async with websockets.serve(hand_landmarks, "localhost", 3000):
+        await asyncio.Future()
+
+asyncio.run(main())
