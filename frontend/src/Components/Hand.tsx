08e48fbe55fba275b1a2c6d9e4835d45a7ec6464
c4bae <123281198+c4bae@users.noreply.github.com>
2025-10-20 14:22:30 -0400

    implement handlines into scene

--- frontend/src/Components/Hand.tsx
+++ frontend/src/Components/Hand.tsx
@@ -3,6 +3,7 @@ import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import React from "react";
 import { RigidBody, RapierRigidBody } from "@react-three/rapier";
+import HandLines from "./HandLines";
 
 interface handLandmarkData {
     landmarks: number[][]
@@ -10,9 +11,11 @@ interface handLandmarkData {
 export default function Hand() {
     const handLandmarkRef = useHandData()
     const spheresRef = useRef<React.RefObject<RapierRigidBody | null>[]>([])
-    const landmarkData = useRef<handLandmarkData | null>(null);
+    const landmarkData = useRef<handLandmarkData | null>(null)
 
     // Build the 21 refs for each hand landmark as part of a 21-element long array
+    // Note that we donʻt place this code in a useEffect hook as it occurs after the first render, 
+    // meaning the refs wonʻt exist when the 21 rigidbodys are rendered (prev mistake)
     spheresRef.current = Array(21)
         .fill(0)
         .map((_, i) => {
@@ -25,11 +28,11 @@ export default function Hand() {
 
             landmarkData.current["landmarks"].forEach((list, index) => {
                     const x = -(list[0] - 1280/2) / (1280/2) * 3
-                    const y = -(list[1] - 720/2) / (720/2) + 0.5
+                    const y = -(list[1] - 720/2) / (720/2) + 1
                     const z = list[2] / 200
 
                     // Update the position of each landmark using kinematic translation
-                    spheresRef.current[index]?.current?.setNextKinematicTranslation({x:x, y:y, z:z})
+                    spheresRef.current[index]?.current?.setNextKinematicTranslation({x,y,z})
             })
         }
     })
@@ -44,6 +47,7 @@ export default function Hand() {
                     </mesh>
                 </RigidBody>
             )}
+            <HandLines landMarkData={landmarkData}></HandLines>
         </>
     )
 }
---
+++ frontend/src/Components/HandLines.tsx
@@ -0,0 +1,87 @@
+import { useState } from "react"
+import { Line } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import React from "react";
+
+interface handLandmarkData {
+    landmarks: number[][]
+}
+
+type landmarkData = {
+    landMarkData: React.RefObject<handLandmarkData>,
+}
+
+export default function HandLines({landMarkData}: landmarkData) {
+    const [stateLandMarkData, setstateLandMarkData] = useState<handLandmarkData | null>(null)
+    
+    useFrame(() => {
+        setstateLandMarkData(landMarkData.current)
+    })
+
+    return (
+        <>
+            {[...Array(20)].map((_, index) => {
+                if (stateLandMarkData) {
+                    const x = -(stateLandMarkData["landmarks"][index][0] - 1280/2) / (1280/2) * 3
+                    const y = -(stateLandMarkData["landmarks"][index][1] - 720/2) / (720/2) + 1
+                    const z = stateLandMarkData["landmarks"][index][2] / 200
+
+                    const nextX = -(stateLandMarkData["landmarks"][index+1][0] - 1280/2) / (1280/2) * 3
+                    const nextY = -(stateLandMarkData["landmarks"][index+1][1] - 720/2) / (720/2) +1
+                    const nextZ = stateLandMarkData["landmarks"][index+1][2] / 200
+
+                    if (index == 0) {
+                        return ( <> 
+                            <Line points={[[x,y,z],[nextX,nextY,nextZ]]} lineWidth={5} color={"blue"}></Line>
+                            <Line points={[[x,y,z], [
+                                -(stateLandMarkData["landmarks"][index+1][0] - 1280/2) / (1280/2) * 3,
+                                -(stateLandMarkData["landmarks"][index+1][1] - 720/2) / (720/2) +1,
+                                stateLandMarkData["landmarks"][index+1][2] / 200
+                            ]]} lineWidth={5} color={"blue"}> </Line>
+
+                            <Line points={[[x,y,z], [
+                                -(stateLandMarkData["landmarks"][index+5][0] - 1280/2) / (1280/2) * 3,
+                                -(stateLandMarkData["landmarks"][index+5][1] - 720/2) / (720/2) +1,
+                                stateLandMarkData["landmarks"][index+5][2] / 200
+                            ]]} lineWidth={5} color={"blue"}> </Line>
+
+                            <Line points={[[x,y,z], [
+                                -(stateLandMarkData["landmarks"][index+9][0] - 1280/2) / (1280/2) * 3,
+                                -(stateLandMarkData["landmarks"][index+9][1] - 720/2) / (720/2) +1,
+                                stateLandMarkData["landmarks"][index+9][2] / 200
+                            ]]} lineWidth={5} color={"blue"}> </Line>
+
+                            <Line points={[[x,y,z], [
+                                -(stateLandMarkData["landmarks"][index+13][0] - 1280/2) / (1280/2) * 3,
+                                -(stateLandMarkData["landmarks"][index+13][1] - 720/2) / (720/2) +1,
+                                stateLandMarkData["landmarks"][index+13][2] / 200
+                            ]]} lineWidth={5} color={"blue"}> </Line>
+
+                            <Line points={[[x,y,z], [
+                                -(stateLandMarkData["landmarks"][index+17][0] - 1280/2) / (1280/2) * 3,
+                                -(stateLandMarkData["landmarks"][index+17][1] - 720/2) / (720/2) +1,
+                                stateLandMarkData["landmarks"][index+17][2] / 200
+                            ]]} lineWidth={5} color={"blue"}> </Line>
+                            </>)
+                    }
+
+                    // Ensure spheres at the bottom of each "finger" connect
+                    if (index == 5 || index == 9 || index == 13 ) {
+                        return ( <> 
+                            <Line points={[[x,y,z],[nextX,nextY,nextZ]]} lineWidth={5} color={"blue"}></Line>
+                            <Line points={[[x,y,z], [
+                                -(stateLandMarkData["landmarks"][index+4][0] - 1280/2) / (1280/2) * 3,
+                                -(stateLandMarkData["landmarks"][index+4][1] - 720/2) / (720/2) +1,
+                                stateLandMarkData["landmarks"][index+4][2] / 200
+                            ]]} lineWidth={5} color={"blue"}> </Line>
+                            </>)
+                    }
+                    // Ensure spheres at the top of each "finger" do not have a connecting line
+                    if (index % 4 != 0)
+                    return <Line points={[[x,y,z],[nextX,nextY,nextZ]]} lineWidth={5} color={"blue"}></Line>
+                }    
+            })}
+            {}
+        </>
+    )
+}
