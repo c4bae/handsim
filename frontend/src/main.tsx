6c521fd0f7dfb78b21c50750f7e5fa5e8e3fc41d
c4bae <123281198+c4bae@users.noreply.github.com>
2025-10-20 22:30:46 -0400

    add perf to project

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.tsx'
 import HandVideo from "./Components/HandVideo.tsx"
 import "./style.css"
+import { Perf } from 'r3f-perf'
 
 
 createRoot(document.getElementById('root')!).render(
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
     <h1>MediaPipe HandDetector Test in R3F<br></br>@c4bae</h1>
     <HandVideo></HandVideo>
     <Canvas shadows>
+        <Perf position="bottom-left"></Perf>
         <Experience></Experience>
     </Canvas>
   </>
