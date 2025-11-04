d80f42df961f4b6aa3a3abe0880d8bddc0424d18
c4bae <123281198+c4bae@users.noreply.github.com>
2025-11-03 19:03:08 -0500

    adjust y position of hand again

---
+++ frontend/src/Components/Barrier.tsx
@@ -0,0 +1,6 @@
+export default function Barrier() {
+    <mesh>
+        <boxGeometry></boxGeometry>
+        <meshStandardMaterial color={"green"} opacity={0.5} transparent></meshStandardMaterial>
+    </mesh>
+}
--- frontend/src/Components/Hand.tsx
+++ frontend/src/Components/Hand.tsx
@@ -42,7 +42,7 @@ export default function Hand() {
 
             landmarkData.current["landmarks"].forEach((list, index) => {
                     const x = -(list[0] - 1280/2) / (1280/2) * 3
-                    const y = -(list[1] - 720/2) / (720/2) * 1.5 + 3.5
+                    const y = -(list[1] - 720/2) / (720/2) * 1.5 + 4
                     const z = list[2] / 200 + zFactor.current
 
                     // Update the position of each landmark using kinematic translation
@@ -52,8 +52,8 @@ export default function Hand() {
     })
     
     return (
-        <>
-            {[...Array(21)].map((_, index) =>
+        <> 
+            {[...Array(21)].map((_, index) => 
                 <RigidBody colliders={false} key={index} ref={spheresRef.current[index] } type={"kinematicPosition"}>
                     <mesh scale={0.06} castShadow>
                         <sphereGeometry></sphereGeometry>
